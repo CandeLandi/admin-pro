a751748908e3a7adce2378809061dba04d619fa7
Candela Landi <landicandela01@gmail.com>
2024-10-08 07:19:57 -0300

    updated .gitignore

--- src/app/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-// TODO: cambiar url
-
-export const environment = {
-  production: true,
-  base_url: 'https://adminpro-backend1-993bf28005ef.herokuapp.com/api'
-}
--- src/app/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-
-export const environment = {
-  production: false,
-  base_url: 'https://adminpro-backend1-993bf28005ef.herokuapp.com/api'
-}
